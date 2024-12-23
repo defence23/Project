994fd78635f9cc2802434b05cac18ae172a949af
defence23 <vasia.pricop00@mail.ru>
2024-12-23 19:49:13 +0200

    users routes done

--- src/schemas.py
+++ src/schemas.py
@@ -1,11 +1,8 @@
 from pydantic import BaseModel, EmailStr
-
 class UserBase(BaseModel):
     email: EmailStr
     username: str
-
 class UserCreate(UserBase):
     password: str
-
 class User(UserBase):
-    id : int
+    id: int
